0febeb79229828274c7fd689ae2e2c73b5ba0906
Hitesh Rathi <97667730+HiteshRathi02@users.noreply.github.com>
2025-05-31 14:30:17 +0000

    Passing props

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,14 @@
+import React, { useState } from "react";
+import "../styles/Child.css"
 
+export default function Selection({applyColor}){
+    const [style, setStyle] = useState({background: ""})
+
+    return(
+    <>
+    <div className="fix-box" style={style}  onClick={()=>applyColor(setStyle)}>
+        Selection
+    </div>
+    </>
+    )
+}
