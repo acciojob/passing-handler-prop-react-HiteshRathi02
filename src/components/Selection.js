361d5023c980d65321f363b4b72f0333b01c0618
Hitesh Rathi <97667730+HiteshRathi02@users.noreply.github.com>
2025-05-31 14:35:51 +0000

    key attached to box

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import "../styles/Child.css"
 
-export default function Selection({applyColor}){
+export default function Selection({ key,applyColor}){
     const [style, setStyle] = useState({background: ""})
 
     return(
     <>
-    <div className="fix-box" style={style}  onClick={()=>applyColor(setStyle)}>
+    <div className="fix-box" style={style} key={key} onClick={()=>applyColor(setStyle)}>
         Selection
     </div>
     </>
